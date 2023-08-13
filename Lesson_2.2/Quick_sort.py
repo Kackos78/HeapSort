de6d725330f8bed16bea0b192036d3f496d35005
Kackos78 <89691782+Kackos78@users.noreply.github.com>
2023-08-13 16:21:07 +0300

    Delete Quick_sort.py

--- Lesson_2.2/Quick_sort.py
+++
@@ -1,9 +0,0 @@
-#  Быстрая сортировка
-def quick_sort(lst):
-    if len(lst) <= 1:
-        return lst
-    pivot = lst[len(lst) // 2]
-    left = list(filter(lambda x: x < pivot, lst))
-    center = [i for i in lst if i == pivot]
-    right = list(filter(lambda x: x > pivot, lst))
-    return quick_sort(left) + center + quick_sort(right)
